290058de86d0dd5a8db47635e578565995e7cf82
Alle SECK <alleseck66@gmail.com>
2024-05-16 00:54:13 +0200

    backend and frontend separed

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const dotenv = require('dotenv');
+
+const dev = process.env.NODE_ENV !== 'production';
+
+dotenv.config();
+if (!process.env.JWT_PRIVATE_KEY) {
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
+}
+
+mongoose.connect('mongodb://localhost/gym')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+  const server = express();
+
+  server.use(express.json());
+
+  server.use('/api/users', users);
+  server.use('/api/auth', auth);
+
+  
+
+  const port = process.env.PORT || 3001;
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+
